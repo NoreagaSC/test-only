d2f22f6bfabe580afcd1e84d28ac2ab0869726cf
Yuriy <kim.u.907@gmail.com>
2025-03-07 22:12:18 +0300

    master Добавлена анимация вращения круга, привязанная к изменению периода

--- src/components/RotatingCircle/RotatingCircle.tsx
+++ src/components/RotatingCircle/RotatingCircle.tsx
@@ -1,3 +1,6 @@
+import { useGSAP } from '@gsap/react';
+import { BASE_ROTATION_STEP } from 'components/RotatingCircle/constants';
+import gsap from 'gsap';
 import React, {
   type Dispatch,
   type FC,
@@ -23,12 +26,30 @@ export const RotatingCircle: FC<IProps> = ({
   maxSteps,
 }): ReactElement => {
   const [hoveredDot, setHoveredDot] = useState<number | null>(0);
+  const prevActivePeriod = useRef<number>(activePeriod);
 
   const dots = generateDots(DOTS_COUNT, Dot, hoveredDot, setHoveredDot);
 
   const circleRef = useRef<HTMLDivElement>(null);
 
-  console.log(activePeriod, maxSteps);
+  console.log(maxSteps);
+
+  useGSAP(() => {
+    if (prevActivePeriod.current !== activePeriod) {
+      const delta = activePeriod - prevActivePeriod.current;
+      const direction = delta > 0 ? 1 : -1;
+      const rotationAngle = Math.abs(delta) * BASE_ROTATION_STEP;
+
+      gsap.to(circleRef.current, {
+        rotation: `+=${rotationAngle * direction}`,
+        duration: 2,
+        ease: 'power2.out',
+        transformOrigin: 'center',
+      });
+
+      prevActivePeriod.current = activePeriod;
+    }
+  }, [activePeriod]);
 
   return <StyledCircle ref={circleRef}>{dots}</StyledCircle>;
 };
--- src/components/RotatingCircle/constants.ts
+++ src/components/RotatingCircle/constants.ts
@@ -6,3 +6,5 @@ export const RADIUS = CIRCLE_SIZE / 2;
 export const ANGLE_STEP = (2 * Math.PI) / DOTS_COUNT;
 
 export const FIXED_ACTIVE_INDEX = 0;
+
+export const BASE_ROTATION_STEP = 360 / DOTS_COUNT;
